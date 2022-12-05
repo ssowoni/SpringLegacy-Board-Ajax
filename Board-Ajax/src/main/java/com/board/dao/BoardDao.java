3455af482328fbb0346d26443b0039d03365e246
sowon <thdnjs0946@naver.com>
2022-12-05 15:45:55 +0900

    게시판 CRUD 완료

--- Board-Ajax/src/main/java/com/board/dao/BoardDao.java
+++ Board-Ajax/src/main/java/com/board/dao/BoardDao.java
@@ -1,23 +1,20 @@
 package com.board.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.board.domain.BoardVO;
 
 /*@Mapper*/
 public interface BoardDao {
 	
-	public List<BoardVO> getList(BoardVO vo) throws Exception;
+	public List<BoardVO> getList(Map<String, Object> map) throws Exception;
 	
-	public List<BoardVO> readPost(int no) throws Exception;
+	public BoardVO readPost(int no) throws Exception;
 	
 	public void insertPost(BoardVO board) throws Exception; 
 	public void deletePost(int no);
 	public void updatePost(BoardVO board);
 	
-	/*
-	 * public void readCount(BoardVO board);
-	 * 
-	 * public List<BoardVO> sortNo(BoardVO board) throws Exception;
-	 */
+
 }
